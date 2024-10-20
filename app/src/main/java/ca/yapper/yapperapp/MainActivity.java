e20bdfd4c715ae5dfb3487faf4421e44a2e33db6
Mohammedmostain <122962236+Mohammedmostain@users.noreply.github.com>
2024-10-19 19:26:25 -0600

    made fragment java file and fixed bugs in xml

--- app/src/main/java/ca/yapper/yapperapp/MainActivity.java
+++ app/src/main/java/ca/yapper/yapperapp/MainActivity.java
@@ -1,12 +1,14 @@
 package ca.yapper.yapperapp;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+import ca.yapper.yapperapp.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Set padding for system bars (status and navigation bars)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Load the ProfileFragment into the fragment container
+        if (savedInstanceState == null) {
+            ProfileFragment profileFragment = new ProfileFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, profileFragment);
+            transaction.commit();
+        }
     }
 }
---
+++ app/src/main/java/ca/yapper/yapperapp/ProfileFragment.java
@@ -0,0 +1,21 @@
+package ca.yapper.yapperapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the fragment layout
+        return inflater.inflate(R.layout.fragment_profile_entrant, container, false);
+    }
+}
